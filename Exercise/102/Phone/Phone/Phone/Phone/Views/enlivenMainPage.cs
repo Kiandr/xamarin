ab0eeb678aab0886a857293c4fe59812e2036011
kiandr <davoudi.rk@gmail.com>
2018-05-07 17:40:11 -0700

    hello world Xamarin form.

---
+++ Exercise/102/Phone/Phone/Phone/Phone/Views/enlivenMainPage.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace Phone
+{
+    public class MainEnlivenPage : MainPage
+    {
+        private ContentPage StartPage
+        {
+            get
+            {
+                var mainEnlivenPage = this;
+                return mainEnlivenPage;
+            }
+        }
+
+        public MainEnlivenPage()
+        {
+             this.StartPage.Content = new Label() { Text = "Welcome John", MinimumHeightRequest = 40, AnchorX = 0, AnchorY = 0, BackgroundColor = Color.Blue, FontSize = 40 };
+        }
+
+        public ContentPage GetStartPage()
+        {
+            return StartPage;
+        }
+    } 
+
+}
