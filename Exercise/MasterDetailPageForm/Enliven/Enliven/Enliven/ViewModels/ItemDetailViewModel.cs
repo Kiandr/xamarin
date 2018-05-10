300948cf7bb904dc8c02c8b855f49a6edccef18d
kiandr <davoudi.rk@gmail.com>
2018-05-10 08:24:25 -0700

    Adding Master Detail Page in XML and CS format.

---
+++ Exercise/MasterDetailPageForm/Enliven/Enliven/Enliven/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Enliven.Models;
+
+namespace Enliven.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
