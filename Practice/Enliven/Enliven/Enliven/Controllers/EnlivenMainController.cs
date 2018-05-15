d4fdfb50f53602154b6ca8bb8a14167c92230467
Kian Davoudi-Rad <kiandr@Kians-MBP.localdomain>
2018-05-15 15:20:22 -0700

    adding new material as well as Enliven first page

--- Practice/Enliven/Enliven/Enliven/Controllers/EnlivenMainController.cs
+++ Practice/Enliven/Enliven/Enliven/Controllers/EnlivenMainController.cs
@@ -2,48 +2,90 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Reflection.Emit;
+
 namespace Enliven.Controllers
 {
-    class EnlivenMainController
-    {
-        private Page EnlivenMainPage { get; set; }
+	class EnlivenMainController
+	{
+		private Page EnlivenMainPage { get; set; }
 
-        public Page GetEnlivenMainPage()
-        {
-            return EnlivenMainPage;
-            ;
-        }
+		public Page GetEnlivenMainPage()
+		{
+			return EnlivenMainPage;
+			;
+		}
 
-        public EnlivenMainController()
-        {
+		public EnlivenMainController()
+		{
 
-         //   EnlivenMainPage = new ContentPage() { Content = new Label() { Text = "Hello Ke" } };
-            var layout = new StackLayout();
-            var button = new Button
-            {
-                Text = "StackLayout",
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-            var yellowBox = new BoxView { Color = Color.Yellow, VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
-            var greenBox = new BoxView { Color = Color.Green, VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
-            var blueBox = new BoxView
-            {
-                Color = Color.Blue,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                HeightRequest = 75
-            };
+			//   EnlivenMainPage = new ContentPage() { Content = new Label() { Text = "Hello Ke" } };
+			var enlivenLogo = new Image
+			{
+				Source = ImageSource.FromResource("Enliven.Resources.reverse_mark.png")
+			};
 
-            layout.Children.Add(button);
-            layout.Children.Add(yellowBox);
-            layout.Children.Add(greenBox);
-            layout.Children.Add(blueBox);
-            layout.Spacing = 10;
+			var layout = new StackLayout();
+			var button = new Button
+			{
+				Text = "StackLayout",
+				VerticalOptions = LayoutOptions.Start,
+				HorizontalOptions = LayoutOptions.FillAndExpand
+			};
+			var UpperGreenColour = new BoxView
+			{
+				Color = Color.FromRgb(61, 70, 67),
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand
+			};
+			var LowerGreenColour = new BoxView
+			{
+				Color = Color.FromRgb(61, 70, 67),
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 75,
+			};
 
-            
-            EnlivenMainPage = new ContentPage() {Content =  layout};
-        }
 
-    }
+			var label = new Xamarin.Forms.Label()
+			{
+				Text = "Tap here to start",
+
+				TextColor = Color.FromHex("#ffffff"),
+				FontSize = 20,
+				BackgroundColor = Color.FromHex("#3d4643"),
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 75,
+				HorizontalTextAlignment = TextAlignment.Center,
+				VerticalTextAlignment = TextAlignment.Center
+
+			};
+
+
+			//layout.Children.Add(label);
+
+			layout.Children.Add(UpperGreenColour);
+			layout.Children.Add(enlivenLogo);
+			layout.Children.Add(label);
+			layout.Spacing = 0.01;
+
+
+			//EnlivenMainPage = new ContentPage()
+			//{
+			//	//Content = new DatePicker
+			//	//{
+			//	//	MinimumDate = new DateTime(2018, 1, 1),
+			//	//	MaximumDate = new DateTime(2018, 12, 31),
+			//	//	Date = new DateTime(2018, 6, 21)
+			//	//},
+			//	//BackgroundColor = Color.GreenYellow
+			//	Content = new Image { Source = ImageSource.FromResource("Enliven.Resources.reverse_mark.png") }
+			//};
+			EnlivenMainPage = new ContentPage() { Content = layout };
+		}
+	}
 }
+
